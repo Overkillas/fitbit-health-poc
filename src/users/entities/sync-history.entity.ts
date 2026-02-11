6eaa1650102e0ba70951a56a0a9eb7bc33945971
Overkillas <kaikepn2@gmail.com>
2026-02-11 13:10:47 -0300

    feat: add SyncHistory entity for device sync tracking

---
+++ src/users/entities/sync-history.entity.ts
@@ -0,0 +1,39 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  JoinColumn,
+  CreateDateColumn,
+  Index,
+} from 'typeorm';
+import { User } from './user.entity';
+
+@Entity('sync_history')
+export class SyncHistory {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Index('idx_sync_user_id')
+  @Column()
+  userId: number;
+
+  @ManyToOne(() => User, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'userId' })
+  user: User;
+
+  @Column({ type: 'timestamp' })
+  syncTime: Date;
+
+  @Column({ nullable: true })
+  deviceName: string;
+
+  @Column({ nullable: true })
+  deviceType: string;
+
+  @Column({ nullable: true })
+  battery: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+}
